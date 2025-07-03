d6374509fb3b99c0647271c2de3f5ea5bd5f6c1c
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-07-03 08:35:13 +0700

    initila web

---
+++ fuse-starter-v17.2.0/src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,67 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'apps.scrumboard',
+        title: 'Scrumboard',
+        type : 'basic',
+        icon : 'heroicons_outline:view-boards',
+        link : '/scrumboard'
+    },
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'apps.scrumboard',
+        title: 'Scrumboard',
+        type : 'basic',
+        icon : 'heroicons_outline:view-boards',
+        link : '/scrumboard'
+    },
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'apps.scrumboard',
+        title: 'Scrumboard',
+        type : 'basic',
+        icon : 'heroicons_outline:view-boards',
+        link : '/scrumboard'
+    },
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'apps.scrumboard',
+        title: 'Scrumboard',
+        type : 'basic',
+        icon : 'heroicons_outline:view-boards',
+        link : '/scrumboard'
+    },
+];
