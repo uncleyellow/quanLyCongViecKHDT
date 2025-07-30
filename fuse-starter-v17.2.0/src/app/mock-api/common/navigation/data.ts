5a8fd81ec6962f16572b40aa1a75701e8edd7b3f
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-07-30 10:51:12 +0700

    suẳ đoỏi tiếng việt

--- fuse-starter-v17.2.0/src/app/mock-api/common/navigation/data.ts
+++ fuse-starter-v17.2.0/src/app/mock-api/common/navigation/data.ts
@@ -4,14 +4,14 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 export const defaultNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Dash Board công việc',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
     },
     {
         id   : 'apps.scrumboard',
-        title: 'Scrumboard',
+        title: 'Bảng Kanban quản lý công việc',
         type : 'basic',
         icon : 'heroicons_outline:view-boards',
         link : '/scrumboard'
@@ -20,14 +20,14 @@ export const defaultNavigation: FuseNavigationItem[] = [
 export const compactNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Dash Board công việc',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
     },
     {
         id   : 'apps.scrumboard',
-        title: 'Scrumboard',
+        title: 'Bảng Kanban quản lý công việc',
         type : 'basic',
         icon : 'heroicons_outline:view-boards',
         link : '/scrumboard'
@@ -36,14 +36,14 @@ export const compactNavigation: FuseNavigationItem[] = [
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Dash Board công việc',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
     },
     {
         id   : 'apps.scrumboard',
-        title: 'Scrumboard',
+        title: 'Bảng Kanban quản lý công việc',
         type : 'basic',
         icon : 'heroicons_outline:view-boards',
         link : '/scrumboard'
@@ -52,14 +52,14 @@ export const futuristicNavigation: FuseNavigationItem[] = [
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Dash Board công việc',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/example'
     },
     {
         id   : 'apps.scrumboard',
-        title: 'Scrumboard',
+        title: 'Bảng Kanban quản lý công việc',
         type : 'basic',
         icon : 'heroicons_outline:view-boards',
         link : '/scrumboard'
