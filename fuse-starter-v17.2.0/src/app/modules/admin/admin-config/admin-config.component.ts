b5787605aed3c0b3a9dbb8d09fc3d14f62ceda67
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-07-30 14:39:13 +0700

    thêm route quản trị web cho admin

---
+++ fuse-starter-v17.2.0/src/app/modules/admin/admin-config/admin-config.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'admin-config',
+    templateUrl: './admin-config.component.html',
+    styleUrls: ['./admin-config.component.scss']
+})
+export class AdminConfigComponent implements OnInit {
+    constructor(
+
+    ) {
+
+    }
+
+    ngOnInit(): void {
+
+    }
+
+
+} 
